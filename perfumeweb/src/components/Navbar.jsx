3e258ca56e4f2143a9aac36ea2333e9be9970d85
udaymariserla96 <udaymariserla96gmail.com>
2026-04-04 22:12:08 +0530

    Fix: navbar.css import

--- perfumeweb/src/components/Navbar.jsx
+++ perfumeweb/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useAuth } from "../context/AuthContext";
-import "../styles/Navbar.css";
+import "../styles/navbar.css";
 
 function Navbar() {
   const { isLoggedIn, username, logout } = useAuth();
